7d6325c6f93175a380f2c0bc78ff37ee7c049fdf
Maiadee <maiadeemmony@hotmail.co.uk>
2025-03-06 11:40:43 +0000

    added seed files

--- project/settings.py
+++ project/settings.py
@@ -103,10 +103,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'rde',
-    'HOST': 'localhost',
-    'PORT': 5432
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('PGDATABASE'),
+        'HOST': env('PGHOST'),
+        'USER': env('PGUSER'),
+        'PASSWORD': env('PGPASSWORD'),
+        'PORT': 5432
     }
 }
 
